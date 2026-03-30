1c51f18600850a40a1a99cafd3886bb79542483c
arsmoriendy <nugrahasteven@gmail.com>
2026-03-30 17:17:06 +0700

    chore: prettier format backend

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,6 +1,6 @@
 // db.js
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
 
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
@@ -9,20 +9,20 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+  if (dbInstance) {
+    return dbInstance;
+  }
 
-    const client = new MongoClient(url);      
+  const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+  // Task 1: Connect to MongoDB
+  // {{insert code}}
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+  // Task 2: Connect to database giftDB and store in variable dbInstance
+  //{{insert code}}
 
-    // Task 3: Return database instance
-    // {{insert code}}
+  // Task 3: Return database instance
+  // {{insert code}}
 }
 
 module.exports = connectToDatabase;
