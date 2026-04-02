80b8c2cc9447f6581bddbbf11b1f24e1872e4da1
arsmoriendy <nugrahasteven@gmail.com>
2026-04-02 11:08:44 +0700

    chore: format front-end using prettier

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
 export default function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">GiftLink</a>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        GiftLink
+      </a>
 
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    {/* Task 1: Add links to Home and Gifts below*/}
-                </ul>
-            </div>
-        </nav>
-    );
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          {/* Task 1: Add links to Home and Gifts below*/}
+        </ul>
+      </div>
+    </nav>
+  );
 }
