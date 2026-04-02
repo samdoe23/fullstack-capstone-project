a527a2cea7f4629354aa4cf2b539d9a478cb0877
arsmoriendy <nugrahasteven@gmail.com>
2026-04-02 10:46:49 +0700

    feat: search endpoint

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -7,6 +7,7 @@ const pinoLogger = require("./logger");
 const connectToDatabase = require("./models/db");
 const { loadData } = require("./util/import-mongo/index");
 const giftRoutes = require("./routes/giftRoutes");
+const searchRoutes = require("./routes/searchRoutes");
 
 const app = express();
 app.use("*", cors());
@@ -26,7 +27,7 @@ app.use(express.json());
 app.use("/api/gifts", giftRoutes);
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+app.use("/api/search", searchRoutes);
 
 const pinoHttp = require("pino-http");
 const logger = require("./logger");
