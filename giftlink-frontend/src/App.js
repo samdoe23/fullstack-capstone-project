def1c2289a3c34bbb89d813b61004914651260c7
arsmoriendy <nugrahasteven@gmail.com>
2026-04-03 10:30:42 +0700

    feat: fe update profile

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import MainPage from "./components/MainPage/MainPage";
 import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
+import Profile from "./components/Profile/Profile";
 import SearchPage from "./components/SearchPage/SearchPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -20,6 +21,7 @@ function App() {
         <Route path="/app/login" element={<LoginPage />} />
         <Route path="/app/register" element={<RegisterPage />} />
         <Route path="/app/search" element={<SearchPage />} />
+        <Route path="/app/profile" element={<Profile />} />
       </Routes>
     </>
   );
