dd378f646d8cb1336b485648f52244f25d9abb5f
arsmoriendy <nugrahasteven@gmail.com>
2026-04-03 10:18:29 +0700

    feat: fe register

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-
+import { urlConfig } from "../../config";
+import { useAppContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 import "./RegisterPage.css";
 
 function RegisterPage() {
@@ -7,9 +9,43 @@ function RegisterPage() {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showerr, setShowerr] = useState("");
+  const navigate = useNavigate();
+  const { setIsLoggedIn } = useAppContext();
 
   const handleRegister = async () => {
-    console.log("Register invoked");
+    const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+      //Step 1 - Task 6
+      method: "POST",
+      //Step 1 - Task 7
+      headers: {
+        "content-type": "application/json",
+      },
+      //Step 1 - Task 8
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      }),
+    });
+
+    const json = await response.json();
+    console.log("json data", json);
+    console.log("er", json.error);
+
+    if (json.authtoken) {
+      sessionStorage.setItem("auth-token", json.authtoken);
+      sessionStorage.setItem("name", firstName);
+      sessionStorage.setItem("email", json.email);
+
+      setIsLoggedIn(true);
+
+      navigate("/app");
+    }
+    if (json.error) {
+      setShowerr(json.error);
+    }
   };
 
   return (
@@ -61,6 +97,7 @@ function RegisterPage() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
+              <div className="text-danger">{showerr}</div>
             </div>
 
             <div className="mb-4">
