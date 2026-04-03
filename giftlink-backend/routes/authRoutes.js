078ab56c05f789cdee06e2b742854c9956496b91
arsmoriendy <nugrahasteven@gmail.com>
2026-04-03 10:21:40 +0700

    feat: be login

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const app = express();
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { body, validationResult } = require("express-validator");
 const connectToDatabase = require("../models/db");
 const router = express.Router();
 const dotenv = require("dotenv");
@@ -24,6 +22,11 @@ router.post("/register", async (req, res) => {
 
     //Task 3: Check for existing email
     const existingEmail = await collection.findOne({ email: req.body.email });
+
+    if (existingEmail) {
+      logger.error("Email id already exists");
+      return res.status(400).json({ error: "Email id already exists" });
+    }
 
     const salt = await bcryptjs.genSalt(10);
     const hash = await bcryptjs.hash(req.body.password, salt);
@@ -49,6 +52,46 @@ router.post("/register", async (req, res) => {
     res.json({ authtoken, email });
   } catch (e) {
     return res.status(500).send("Internal server error");
+  }
+});
+
+//Login Endpoint
+router.post("/login", async (req, res) => {
+  console.log("\n\n Inside login");
+
+  try {
+    // const collection = await connectToDatabase();
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+    const theUser = await collection.findOne({ email: req.body.email });
+
+    if (theUser) {
+      let result = await bcryptjs.compare(req.body.password, theUser.password);
+      if (!result) {
+        logger.error("Passwords do not match");
+        return res.status(404).json({ error: "Wrong pasword" });
+      }
+      let payload = {
+        user: {
+          id: theUser._id.toString(),
+        },
+      };
+
+      const userName = theUser.firstName;
+      const userEmail = theUser.email;
+
+      const authtoken = jwt.sign(payload, JWT_SECRET);
+      logger.info("User logged in successfully");
+      return res.status(200).json({ authtoken, userName, userEmail });
+    } else {
+      logger.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+  } catch (e) {
+    logger.error(e);
+    return res
+      .status(500)
+      .json({ error: "Internal server error", details: e.message });
   }
 });
 
